a8586d11710ac6432785ed447d23f33645e1544b
Cristina Onica <onicacristinaelena@yahoo.com>
2023-11-07 12:46:24 +0200

    bottom navigation

---
+++ app/src/main/java/com/example/myapplication/presentation/ui/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (!navController.popBackStack()) {
+                finish()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val graph = navController.navInflater.inflate(R.navigation.mobile_navigation)
+        navController.setGraph(graph, null)
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+        binding.navView.setupWithNavController(navController)
+    }
+}
