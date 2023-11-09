3c340a248acc6b7e7faa049e8b11db40bf530242
Alexandra <alexandra_ch_2010@yahoo.com>
2023-11-09 15:15:06 +0200

    Language Fragment UI

--- app/src/main/java/com/example/myapplication/presentation/settings/select_language/SelectLanguageFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/settings/select_language/SelectLanguageFragment.kt
@@ -1,32 +1,39 @@
 package com.example.myapplication.presentation.settings.select_language
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentSelectLanguageBinding
+import com.example.myapplication.presentation.base.NoBottomNavigationFragment
+import com.example.myapplication.utils.extensions.viewBinding
 
-class SelectLanguageFragment : Fragment() {
+class SelectLanguageFragment :
+    NoBottomNavigationFragment<FragmentSelectLanguageBinding>(R.layout.fragment_select_language) {
 
-    companion object {
-        fun newInstance() = SelectLanguageFragment()
+    override val viewBinding: FragmentSelectLanguageBinding by viewBinding(
+        FragmentSelectLanguageBinding::bind
+    )
+    override val viewModel: SelectLanguageViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        initListeners()
     }
 
-    private lateinit var viewModel: SelectLanguageViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_select_language, container, false)
+    private fun initViews() {
+        initToolbar()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SelectLanguageViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun initToolbar() {
+        viewBinding.toolbar.ivBack.visibility = View.VISIBLE
+        viewBinding.toolbar.tvTitle.setText(R.string.select_language)
     }
 
+    private fun initListeners() {
+        viewBinding.toolbar.ivBack.setOnClickListener {
+            navController.popBackStack()
+        }
+    }
 }
